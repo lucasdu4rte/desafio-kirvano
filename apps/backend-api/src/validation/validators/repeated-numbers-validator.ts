26d5cc030c37bbd2fe27c2962e19afb7a84b7ab0
Lucas Duarte <lucasdu4rte@gmail.com>
2024-04-23 01:21:29 -0300

    feat: remove spaces in cardNumber on repeatedNumbersValidator

--- apps/backend-api/src/validation/validators/repeated-numbers-validator.ts
+++ apps/backend-api/src/validation/validators/repeated-numbers-validator.ts
@@ -2,7 +2,8 @@ import { CardValidator } from "../protocols/card-validator";
 
 export class RepeatedNumbersValidator implements CardValidator {
   validate(cardNumber: string): boolean {
+    const numberWithoutSpaces = cardNumber.replace(/\s/g, '');
     const pattern = /^(\d)\1+$/;
-    return !pattern.test(cardNumber);
+    return !pattern.test(numberWithoutSpaces);
   }
 }
